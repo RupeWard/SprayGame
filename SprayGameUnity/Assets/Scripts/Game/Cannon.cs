dcabadcfb265ff1dd8f28b3abf73832c8e20a0a5
RupeWard <rupe@rupertward.co.uk>
2016-07-28 18:08:17 +0100

    Cannon movement by mouse

--- SprayGameUnity/Assets/Scripts/Game/Cannon.cs
+++ SprayGameUnity/Assets/Scripts/Game/Cannon.cs
@@ -35,6 +35,16 @@ public class Cannon : MonoBehaviour
 		MessageBus.instance.pointerMoveAction -= HandlePointerMove;
 	}
 
+	private void PointAt(Vector2 v)
+	{
+		float angle = Mathf.Rad2Deg * Mathf.Atan2( v.y, v.x );
+		if (DEBUG_CANNON)
+		{
+			Debug.Log( "Cannon Angle is " + angle );
+		}
+
+		cachedTransform_.rotation = Quaternion.Euler( new Vector3( 0f,0f,angle - 90f ) );
+	}
 
 	public void HandlePointerDown(Vector2 v)
 	{
@@ -42,10 +52,12 @@ public class Cannon : MonoBehaviour
 		{
 			Debug.Log( "Cannon: Ptr DOWN at " + v );
 		}
+		PointAt( v );
 	}
 
 	public void HandlePointerUp( Vector2 v )
 	{
+		PointAt( v );
 		if (DEBUG_CANNON)
 		{
 			Debug.Log( "Cannon: Ptr UP at " + v );
@@ -54,6 +66,7 @@ public class Cannon : MonoBehaviour
 
 	public void HandlePointerMove( Vector2 v )
 	{
+		PointAt( v );
 		if (DEBUG_CANNON)
 		{
 			Debug.Log( "Cannon: Ptr MOVE at " + v );
