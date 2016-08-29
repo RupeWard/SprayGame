8e346de11ecf06da222a0d620d668a2c442e1f9b
RupeWard <rupe@rupertward.co.uk>
2016-08-29 14:37:43 +0100

    First version of blob shader, including fixing of mesh uvs

--- SprayGameUnity/Assets/Scripts/Game/Blobs/Blob_SimpleCylinder.cs
+++ SprayGameUnity/Assets/Scripts/Game/Blobs/Blob_SimpleCylinder.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class Blob_SimpleCylinder : Blob
 {
+	#region mesh
+
+	static private Mesh sharedMesh_ = null;
+
+	#endregion mesh
+
 	#region private hooks
 
-	public MeshRenderer myRenderer = null;
+	public MeshRenderer cylinderRenderer;
+	public MeshFilter cylinderMeshFilter;
 
 	public Transform top;
 
@@ -15,10 +22,46 @@ public class Blob_SimpleCylinder : Blob
 	{
 		radius_ = cachedTransform.localScale.x;
 		 
-		cachedMaterial_ = new Material( myRenderer.sharedMaterial );
-		myRenderer.sharedMaterial = cachedMaterial_;
+		cachedMaterial_ = new Material( cylinderRenderer.sharedMaterial );
+		cylinderRenderer.sharedMaterial = cachedMaterial_;
 		cachedTransform.localScale = new Vector3 ( radius, 0.5f, radius);
 
+		if (sharedMesh_ == null)
+		{
+			sharedMesh_ = cylinderMeshFilter.sharedMesh;
+			int nV = sharedMesh_.vertices.Length;
+			int nUV = sharedMesh_.uv.Length;
+			System.Text.StringBuilder sb = new System.Text.StringBuilder( );
+			sb.Append( "Found mesh: nV=" + nV + ", nUV=" + nUV );
+			if (nV == nUV)
+			{
+				for (int i = 0; i < nV; i++)
+				{
+					sb.Append( "\n " + sharedMesh_.vertices[i] + " " + sharedMesh_.uv[i] );
+				}
+				Vector2[] newUV = new Vector2[nV];
+				for (int i = 0; i< nV; i++)
+				{
+					float u = 0f;
+					if (Vector2.Distance(new Vector2( sharedMesh_.vertices[i].z, sharedMesh_.vertices[i].x ), Vector2.zero) < Mathf.Epsilon)
+					{
+						u = 1f;
+					}
+					float angle = Mathf.Atan2( sharedMesh_.vertices[i].z, sharedMesh_.vertices[i].x );
+					while (angle < 0f) angle += 2f * Mathf.PI;
+					float angleFraction = angle / (2f * Mathf.PI);
+					newUV[i] = new Vector2( u, angleFraction );
+				}
+				sharedMesh_.uv = newUV;
+
+			}
+			Debug.LogError( sb.ToString( ) );
+
+		}
+		else
+		{
+			cylinderMeshFilter.sharedMesh = sharedMesh_;
+		}
 		top.gameObject.SetActive( false );
 	}
 
